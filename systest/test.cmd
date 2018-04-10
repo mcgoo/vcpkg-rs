344c2f69f8e2c5e585246f56eb0a68e7261b993b
Jim McGrath <jimmc2@gmail.com>
2018-04-10 13:05:24 -0500

    update test.cmd to use current environment variables

--- systest/test.cmd
+++ systest/test.cmd
@@ -3,15 +3,19 @@
 set VCPKG_PANIC=on
 set VCPKG_ROOT=c:\Users\jim\src\vcpkg
 
-rem  
-for %%i in (x86_64-pc-windows-msvc i686-pc-windows-msvc x86_64-pc-windows-gnu i686-pc-windows-gnu) DO (
-    set VCPKG_ALL_STATIC=on
-    set VCPKG_ALL_DYNAMIC=
+rem for %%i in (x64-windows x64-windows-static x86-windows x86-windows-static) DO (
+rem     %VCPKG_ROOT%\vcpkg install --triplet=%i% openssl curl
+rem )
+
+rem x86_64-pc-windows-gnu i686-pc-windows-gnu
+for %%i in (x86_64-pc-windows-msvc i686-pc-windows-msvc) DO (
+    set RUSTFLAGS=-Ctarget-feature=+crt-static
+    set VCPKGRS_DYNAMIC=
     echo %%i static
     cargo run --target %%i
     cargo clean
-    set VCPKG_ALL_STATIC=
-    set VCPKG_ALL_DYNAMIC=on
+    set RUSTFLAGS=
+    set VCPKGRS_DYNAMIC=1
     echo %%i dynamic
     cargo run --target %%i
     cargo clean
