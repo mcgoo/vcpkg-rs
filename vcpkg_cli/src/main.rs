b7635d1067e8ff11832a8fa704a3400f8f7f150a
Jim McGrath <jimmc2@gmail.com>
2018-06-11 15:50:49 -0500

    use find_library and say "package" instead of "library"

--- vcpkg_cli/src/main.rs
+++ vcpkg_cli/src/main.rs
@@ -19,9 +19,9 @@ fn main() {
         )
         .subcommand(
             SubCommand::with_name("probe")
-                .about("try to find a library")
+                .about("try to find a package")
                 .arg(
-                    Arg::with_name("library")
+                    Arg::with_name("package")
                         .index(1)
                         .required(true)
                         .help("probe for a library and display paths and cargo metadata"),
@@ -41,7 +41,7 @@ fn main() {
     env::set_var("TARGET", matches.value_of("target").unwrap());
 
     if let Some(matches) = matches.subcommand_matches("probe") {
-        let lib_name = matches.value_of("library").unwrap();
+        let lib_name = matches.value_of("package").unwrap();
 
         let mut cfg = vcpkg::Config::new();
         cfg.cargo_metadata(false);
@@ -58,47 +58,47 @@ fn main() {
             }
         }
 
-        match cfg.probe(lib_name) {
+        match cfg.find_package(lib_name) {
             Ok(lib) => {
                 println!("Found library {}", lib_name);
 
                 if !lib.include_paths.is_empty() {
                     println!("Include paths:");
                     for line in &lib.include_paths {
-                        println!("\t{}", line.as_os_str().to_str().unwrap());
+                        println!("  {}", line.as_os_str().to_str().unwrap());
                     }
                 }
 
                 if !lib.link_paths.is_empty() {
                     println!("Library paths:");
                     for line in &lib.link_paths {
-                        println!("\t{}", line.as_os_str().to_str().unwrap());
+                        println!("  {}", line.as_os_str().to_str().unwrap());
                     }
                 }
 
                 if !lib.link_paths.is_empty() {
                     println!("Runtime Library paths:");
                     for line in &lib.dll_paths {
-                        println!("\t{}", line.as_os_str().to_str().unwrap());
+                        println!("  {}", line.as_os_str().to_str().unwrap());
                     }
                 }
 
                 if !lib.cargo_metadata.is_empty() {
                     println!("Cargo metadata:");
                     for line in &lib.cargo_metadata {
-                        println!("\t{}", line);
+                        println!("  {}", line);
                     }
                 }
                 if !lib.found_dlls.is_empty() {
                     println!("Found DLLs:");
                     for line in &lib.found_dlls {
-                        println!("\t{}", line.display());
+                        println!("  {}", line.display());
                     }
                 }
                 if !lib.found_libs.is_empty() {
                     println!("Found libs:");
                     for line in &lib.found_libs {
-                        println!("\t{}", line.display());
+                        println!("  {}", line.display());
                     }
                 }
             }
