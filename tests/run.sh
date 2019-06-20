cd924a496a65e92352ab031fc5262ed8f8d06dca
Jim McGrath <jimmc2@gmail.com>
2019-06-20 14:12:52 -0500

    check for failure when package is not installed

--- tests/run.sh
+++ tests/run.sh
@@ -7,6 +7,8 @@ cd $SCRIPTDIR
 export VCPKG_ROOT=$SCRIPTDIR/../vcp
 
 for port in harfbuzz ; do
+    # check that the port fails before it is installed
+    cargo run --manifest-path $port/Cargo.toml && exit 2
     $VCPKG_ROOT/vcpkg install $port
     cargo run --manifest-path $port/Cargo.toml
 done
