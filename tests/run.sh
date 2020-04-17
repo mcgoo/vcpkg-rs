520d9aa3743e2efd84092c30985ba3c8df0b34ca
Jim McGrath <jimmc2@gmail.com>
2020-04-17 09:07:58 -0500

    log a note after an expected build error in tests

--- tests/run.sh
+++ tests/run.sh
@@ -11,6 +11,8 @@ for port in harfbuzz ; do
     $VCPKG_ROOT/vcpkg remove $port  || true
     cargo clean --manifest-path $port/Cargo.toml
     cargo run --manifest-path $port/Cargo.toml && exit 2
+    echo THIS FAILURE IS EXPECTED
+    echo This is to ensure that we are not spuriously succeding because the libraries already exist someone on the build machine.
     $VCPKG_ROOT/vcpkg install $port
     cargo run --manifest-path $port/Cargo.toml
 done
