7dec717fe04a81d5c916654e3909d716a5126729
Jim McGrath <jimmc2@gmail.com>
2020-05-26 21:11:09 -0500

    print RUSTFLAGS

--- wstest/wstest/build.rs
+++ wstest/wstest/build.rs
@@ -1,4 +1,8 @@
 fn main() {
+    println!(
+        "RUSTFLAGS={}",
+        std::env::var("RUSTFLAGS").unwrap_or_default()
+    );
     // panic!("CARGO={}", std::env::var("CARGO").unwrap());
     vcpkg::find_package("sdl2").unwrap();
 }
