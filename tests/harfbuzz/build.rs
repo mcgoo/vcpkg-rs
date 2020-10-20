77a00d2d1d6d961611a5af90b9145a368ea6c998
Jim McGrath <jimmc2@gmail.com>
2020-10-19 23:03:49 -0500

    work-around for link order issue on linux

--- tests/harfbuzz/build.rs
+++ tests/harfbuzz/build.rs
@@ -2,11 +2,13 @@ extern crate cc;
 extern crate vcpkg;
 
 fn main() {
-    //vcpkg::find_package("harfbuzz").unwrap();
     let libs = vcpkg::Config::new().find_package("harfbuzz").unwrap();
 
-    // println!("{:?}",libs);
-    // panic!();
+    // vcpkg-rs is not capable of working out the correct order to link
+    // libraries in. This only matters on Linux at present. (vcpkg itself
+    // does fine, but vcpkg-rs needs to work out how to get the link order
+    // from the it.)
+    println!("cargo:rustc-link-lib=brotlicommon-static");
 
     let mut build = cc::Build::new();
     build.file("src/test.c");
