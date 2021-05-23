713efe4300f75b964b228a599562f2c0ac06d512
Dirk Stolle <striezel-dev@web.de>
2021-05-23 12:08:11 +0200

    fix some typos

--- tests/run.sh
+++ tests/run.sh
@@ -14,7 +14,7 @@ for port in harfbuzz ; do
     cargo clean --manifest-path $port/Cargo.toml
     cargo run --manifest-path $port/Cargo.toml && exit 2
     echo THIS FAILURE IS EXPECTED
-    echo This is to ensure that we are not spuriously succeding because the libraries already exist someone on the build machine.
+    echo This is to ensure that we are not spuriously succeeding because the libraries already exist somewhere on the build machine.
     $VCPKG_ROOT/vcpkg install $port
     cargo run --manifest-path $port/Cargo.toml
 done
