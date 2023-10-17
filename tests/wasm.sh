102bbb6f0912997a12afd10fb4b376eecab7a6fc
TheVeryDarkness <3266343194@qq.com>
2023-10-18 03:12:54 +0800

    Fixed ci
    Do not run after building wasm target.

--- tests/wasm.sh
+++ tests/wasm.sh
@@ -4,6 +4,8 @@ set -ex
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPTDIR
 
+export CARGO_BUILD_TARGET=wasm32-unknown-unknown
+
 export VCPKG_ROOT=$SCRIPTDIR/../vcp
 
 source ../setup_vcp.sh
@@ -12,11 +14,11 @@ for port in harfbuzz ; do
     # check that the port fails before it is installed
     $VCPKG_ROOT/vcpkg remove $port  || true
     cargo clean --manifest-path $port/Cargo.toml
-    cargo run --manifest-path $port/Cargo.toml && exit 2
+    cargo build --manifest-path $port/Cargo.toml && exit 2
     echo THIS FAILURE IS EXPECTED
     echo This is to ensure that we are not spuriously succeeding because the libraries already exist somewhere on the build machine.
     $VCPKG_ROOT/vcpkg install $port
-    cargo run --manifest-path $port/Cargo.toml
+    cargo build --manifest-path $port/Cargo.toml
 done
 
 
@@ -27,12 +29,12 @@ cargo clean --manifest-path top-level/Cargo.toml
 unset VCPKG_INSTALLED_ROOT
 rm -rf $VCPKG_ROOT/installed
 
-cargo run --manifest-path top-level/Cargo.toml && exit 2
+cargo build --manifest-path top-level/Cargo.toml && exit 2
 echo "This failure is expected, as we haven't installed anything from vcpkg yet."
 
 export VCPKG_INSTALLED_ROOT=$SCRIPTDIR/top-level/vcpkg_installed
 pushd top-level
 $VCPKG_ROOT/vcpkg install
 popd
-cargo run --manifest-path top-level/Cargo.toml
+cargo build --manifest-path top-level/Cargo.toml
 unset VCPKG_INSTALLED_ROOT
