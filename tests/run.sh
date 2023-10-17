7f845a0c8f4cce61d2d30ef197555eddab3ce47d
TheVeryDarkness <3266343194@qq.com>
2023-10-18 01:40:57 +0800

    Create wasm.yml and modify run.sh
    Pass extra args to run.sh.

--- tests/run.sh
+++ tests/run.sh
@@ -11,12 +11,12 @@ source ../setup_vcp.sh
 for port in harfbuzz ; do
     # check that the port fails before it is installed
     $VCPKG_ROOT/vcpkg remove $port  || true
-    cargo clean --manifest-path $port/Cargo.toml
-    cargo run --manifest-path $port/Cargo.toml && exit 2
+    cargo clean $* --manifest-path $port/Cargo.toml
+    cargo run $* --manifest-path $port/Cargo.toml && exit 2
     echo THIS FAILURE IS EXPECTED
     echo This is to ensure that we are not spuriously succeeding because the libraries already exist somewhere on the build machine.
     $VCPKG_ROOT/vcpkg install $port
-    cargo run --manifest-path $port/Cargo.toml
+    cargo run $* --manifest-path $port/Cargo.toml
 done
 
 
