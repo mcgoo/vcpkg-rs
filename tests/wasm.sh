02fe2d8b037b8e79c33d66f1a2b2500e888d731e
TheVeryDarkness <3266343194@qq.com>
2023-10-18 12:20:01 +0800

    Set triplet in ci properly.

--- tests/wasm.sh
+++ tests/wasm.sh
@@ -4,6 +4,8 @@ set -ex
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPTDIR
 
+TRIPLET=wasm32-emscripten
+
 export CARGO_BUILD_TARGET=wasm32-unknown-unknown
 
 export VCPKG_ROOT=$SCRIPTDIR/../vcp
@@ -12,12 +14,12 @@ source ../setup_vcp.sh
 
 for port in harfbuzz ; do
     # check that the port fails before it is installed
-    $VCPKG_ROOT/vcpkg remove $port  || true
+    $VCPKG_ROOT/vcpkg remove $port:$TRIPLET  || true
     cargo clean --manifest-path $port/Cargo.toml
     cargo build --manifest-path $port/Cargo.toml && exit 2
     echo THIS FAILURE IS EXPECTED
     echo This is to ensure that we are not spuriously succeeding because the libraries already exist somewhere on the build machine.
-    $VCPKG_ROOT/vcpkg install $port
+    $VCPKG_ROOT/vcpkg install $port:$TRIPLET
     cargo build --manifest-path $port/Cargo.toml
 done
 
@@ -34,7 +36,7 @@ echo "This failure is expected, as we haven't installed anything from vcpkg yet.
 
 export VCPKG_INSTALLED_ROOT=$SCRIPTDIR/top-level/vcpkg_installed
 pushd top-level
-$VCPKG_ROOT/vcpkg install
+$VCPKG_ROOT/vcpkg install --triplet=$TRIPLET
 popd
 cargo build --manifest-path top-level/Cargo.toml
 unset VCPKG_INSTALLED_ROOT
